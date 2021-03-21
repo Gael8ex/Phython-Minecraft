0e7821dda95cf89f84398afd7f08c6404eab19fe
Gael8ex <75571192+Gael8ex@users.noreply.github.com>
2021-03-21 11:10:29 +0100

    I update the scripts
    :smiley:

--- Scripts/House.py
+++ Scripts/House.py
@@ -1,22 +1,10 @@
 #Connect to Minecraft
 from mcpi.minecraft import Minecraft
 import mcpi.block as block
-
-#Necesitamos importar time
 import time
 
 
 mc = Minecraft.create()
-
-
-
-# Materiales!
-# A todos se vinculó una variable
-# El número es el ID del material
-
-# Para poner variables y numeros: https://minecraft-ids.grahamedgecombe.com
- 
-# Para usar el comando block. : https://www.raspberrypi-spy.co.uk/2014/09/raspberry-pi-minecraft-block-id-number-reference/ 
 
 stone = 1
 cobblestone = 4     #block.STONE
@@ -27,23 +15,16 @@ stone_slab = 44     #block.STONE_SLAB
 glass = 20          #block.GLASS
 aire = 0            #block.AIR
 
-##########################################
-# Primero  determinamos la coordenada inicial
-# Este será nuestro punto de referencia
-x = 240
-y = 0       #podria ser 4 también!
+
+x = 0
+y = 0
 z = 110
 
 mc.setBlocks(x-50, y, z-50, x+50, y+50, z+50, block.AIR)
 
 
 time.sleep(1)
-##########################################
-# Construccion bloque por bloque
-# Tenemos que tener y ver un plano 
-# con solo la imaginacion es dificil pero posible
 
-#Este bloque hace de escalera
 mc.setBlock(x+2, y, z+1, block.WOOD_PLANKS)
 
 time.sleep(1)
